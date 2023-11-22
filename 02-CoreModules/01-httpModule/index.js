723ab9d18fc8dececf7db4583e70ba73d123fa18
eduardkb <eduardbuhali@gmail.com>
2023-11-22 20:39:40 -0300

    http server with HTML

---
+++ 02-CoreModules/01-httpModule/index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.write("Hellow, HTTP.");
+  res.end();
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
