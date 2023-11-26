3233cbe882056ef47253cdb4bacff41008ff3dca
eduardkb <eduardbuhali@gmail.com>
2023-11-26 10:37:27 -0300

    middleware

--- 05_ExpressBasics/04-middleware/index.js
+++ 05_ExpressBasics/04-middleware/index.js
@@ -5,6 +5,21 @@ const port = 3000;
 const path = require("path");
 const basePath = path.join(__dirname, "templates");
 
+// very simple middleware
+// checks if user is authenticated or not.
+const checkAuth = function (req, res, next) {
+  req.authStatus = true;
+  if (req.authStatus) {
+    console.log("User is logged in.");
+    next();
+  } else {
+    console.log("User is not logged in. Please login ");
+    next();
+  }
+};
+
+app.use(checkAuth);
+
 app.get("/", (req, res) => {
   res.sendFile(`${basePath}/index.html`);
 });
