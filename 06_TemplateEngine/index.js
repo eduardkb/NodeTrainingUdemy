df9dea39fbe7616af5e7f3a05b6542789835139b
eduardkb <eduardbuhali@gmail.com>
2023-11-27 14:28:18 -0300

    each and with property

--- 06_TemplateEngine/index.js
+++ 06_TemplateEngine/index.js
@@ -24,8 +24,20 @@ app.get("/", (req, res) => {
   };
   const produto = "Maçã";
   const auth = true;
+  const arrItems = [
+    { name: "Lamp", price: 14.99 },
+    { name: "Webcam", price: 154.0 },
+    { name: "Headset", price: 899.99 },
+  ];
+  const objLecture = {
+    title: "Node.js Handlers",
+    description: "Learn abou handlers in node.js programming language",
+    panelist: "John F. Doe",
+    cost: 1550,
+    location: "Hotel Mabu",
+  };
 
-  res.render("home", { webData: user, produto, auth });
+  res.render("home", { webData: user, produto, auth, arrItems, objLecture });
 });
 
 app.listen(3000, () => {
