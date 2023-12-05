1d400eaaa8d244dcded9a08b03b42694b5be3823
eduardkb <eduardbuhali@gmail.com>
2023-12-05 14:14:34 -0300

    edit tasks

--- 09_MVC/controllers/TaskController.js
+++ 09_MVC/controllers/TaskController.js
@@ -26,4 +26,21 @@ module.exports = class TaskController {
     await Task.destroy({ where: { id: id } });
     res.redirect("/tasks");
   }
+
+  static async updateTask(req, res) {
+    const id = req.params.id;
+    const task = await Task.findOne({ where: { id: id }, raw: true });
+    res.render("tasks/edit", { task });
+  }
+
+  static async updateTaskPost(req, res) {
+    const id = req.body.id;
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+    };
+
+    await Task.update(task, { where: { id: id } });
+    res.redirect("/");
+  }
 };
--- 09_MVC/routes/tasksRoutes.js
+++ 09_MVC/routes/tasksRoutes.js
@@ -6,5 +6,7 @@ router.get("/", TaskController.showTasks);
 router.get("/add", TaskController.createTask);
 router.post("/add", TaskController.createTaskPost);
 router.post("/remove", TaskController.removeTask);
+router.get("/edit/:id", TaskController.updateTask);
+router.post("/edit", TaskController.updateTaskPost);
 
 module.exports = router;
