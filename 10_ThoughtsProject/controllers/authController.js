fea0727ac84682ef1fee739870d7c97c957588a5
eduardkb <eduardbuhali@gmail.com>
2023-12-21 17:22:14 +0000

    Implement flash messages on authController

--- 10_ThoughtsProject/controllers/authController.js
+++ 10_ThoughtsProject/controllers/authController.js
@@ -1,8 +1,23 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
   static login(req, res) {
     res.render("auth/login");
   }
   static register(req, res) {
     res.render("auth/register");
+  }
+  static async registerPost(req, res) {
+    const { name, email, password, confirmPassword } = req.body;
+
+    //password match validation
+    if (password != confirmPassword) {
+      req.flash("message", "The passwords don't match. Try again.");
+      res.render("auth/register");
+      return;
+    }
+
+    res.render("home");
   }
 };
