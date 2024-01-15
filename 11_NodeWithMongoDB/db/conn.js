bd070634d11eeea72a006163ae0230a8e42a55c3
eduardkb <eduardbuhali@gmail.com>
2024-01-15 20:41:58 -0300

    removed db password

--- 11_NodeWithMongoDB/db/conn.js
+++ 11_NodeWithMongoDB/db/conn.js
@@ -1,6 +1,9 @@
 const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb://eduardkb:o1RHi1GByDT7ReKQlsnxhIajdtzTl5LYrsXnvujW2AghS6kaAy7GuR9j1USmWPEfxMvtR5cMNPyWACDbYCVADQ==@eduardkb.mongo.cosmos.azure.com:10255/nodeFirstApp?ssl=true&retrywrites=false&maxIdleTimeMS=120000";
+const pass = "connPass";
+const params = "?ssl=true&retrywrites=false&maxIdleTimeMS=120000";
+const dbName = "nodeFirstApp";
+const uri = `mongodb://eduardkb:${pass}@eduardkb.mongo.cosmos.azure.com:10255/${dbName}${params}`;
+console.log("--> DEB_DB: Connection string:", uri);
 const client = new MongoClient(uri);
 
 async function run() {
