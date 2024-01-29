8a86adde88b48e37c83c8d5c6f1af61d243bbf6c
eduardkb <eduardbuhali@gmail.com>
2024-01-29 16:27:50 -0300

    added css to basic pages

--- 13_API-GetPetProject/frontend/src/App.js
+++ 13_API-GetPetProject/frontend/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Import Components
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/NavbarPg";
+import Footer from "./components/layout/FooterPg";
+import Container from "./components/layout/Container";
 
 // Import Pages
 import Home from "./components/pages/Home";
@@ -14,12 +15,14 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
       <Footer />
     </BrowserRouter>
   );
--- 13_API-GetPetProject/frontend/src/components/layout/FooterPg.js
+++ 13_API-GetPetProject/frontend/src/components/layout/FooterPg.js
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
         <span className="bold">Get a Pet &copy; 2023</span>
       </p>
