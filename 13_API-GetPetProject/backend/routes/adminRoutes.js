b2f40ea0e3bbd6695b69b99bb2e95aef0d1f53df
eduardkb <eduardbuhali@gmail.com>
2024-01-30 18:19:40 -0300

    initial swagger setup

--- 13_API-GetPetProject/backend/routes/adminRoutes.js
+++ 13_API-GetPetProject/backend/routes/adminRoutes.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
 const AdminController = require("../controllers/adminController");
 
+/**
+ * @swagger
+ * /admin/getDbBackup:
+ *   get:
+ *     description: Generate a backup of all users and pets
+ *     responses:
+ *       200:
+ *         description: A list of users and pets
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ */
 router.get("/getDbBackup", AdminController.getDbBackup);
 
 module.exports = router;
