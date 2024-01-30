4cee692fe850f14ef506f398606773f15e3bb8d4
eduardkb <eduardbuhali@gmail.com>
2024-01-30 14:17:53 -0300

    added port env variable

--- 13_API-GetPetProject/backend/index.js
+++ 13_API-GetPetProject/backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const writeLog = require("./helper/write-log");
-const port = 5000;
 const app = express();
 
 // reset log
@@ -16,7 +15,7 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 // Public folder
 app.use(express.static("public"));
 
-// Routes
+// Imported Routes
 const UserRoutes = require("./routes/userRoutes");
 const PetRoutes = require("./routes/petRoutes");
 const AdminRoutes = require("./routes/adminRoutes");
@@ -24,10 +23,19 @@ app.use("/users", UserRoutes);
 app.use("/pets", PetRoutes);
 app.use("/admin", AdminRoutes);
 
+// 404 - default route
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Welcome to the 'Get a Pet' API. This page is not available.",
+  });
+});
+
 // Initialize Server
 try {
+  const port = process.env.PORT || 5000;
   app.listen(port);
   writeLog("DEB", "BeServer", `Server started successfully on port ${port}.`);
+  writeLog("DEB", "BeServer", `Access: https://localhost:${port}/`);
 } catch (error) {
   writeLog("DEB", "BeServer", `Error while starting server: ${error}.`);
 }
